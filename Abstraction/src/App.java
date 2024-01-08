58f406091ffc544a0f5c0fb50ba950d8b6d8fe50
Nada B E J M A Alshaibani <83315@kfh.com>
2024-01-08 09:10:22 +0300

    Abstraction

---
+++ Abstraction/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Teacher razan = new Teacher("Razan", "Physics");
+
+        razan.describeRole();
+    }
+}
---
+++ Abstraction/src/Teacher.java
@@ -0,0 +1,15 @@
+public class Teacher extends Person {
+
+    public String subject;
+
+    public Teacher(String name, String subject) {
+        super(name);
+        this.subject = subject;
+    }
+
+    @Override
+    void describeRole() {
+        System.out.println(getName() + " teaches " + subject);
+    }
+
+}
